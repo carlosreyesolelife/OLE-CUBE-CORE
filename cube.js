761821e1de720bc580de5cd512a0a005e8502901
carlosreyesolelife <carlos.reyes@olelife.com>
2026-03-12 15:44:58 -0500

    finish

--- cube.js
+++ cube.js
@@ -1,19 +1,8 @@
 module.exports = {
-  apiSecret: process.env.CUBEJS_API_SECRET,
-  dbType: 'bigquery',
+  dbType: "bigquery",
 
   driverFactory: () => ({
-    type: 'bigquery',
+    type: "bigquery",
     projectId: process.env.CUBEJS_DB_BQ_PROJECT_ID,
-  }),
-
-  sqlPort: 15432,
-
-  scheduledRefreshContexts: () => [
-    {
-      securityContext: {},
-    },
-  ],
-
-  queryRewrite: (query) => query,
+  })
 };

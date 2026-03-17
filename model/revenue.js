6d8b23331cdd59f4565dbe7738d3678d0b97ce03
carlosreyesolelife <carlos.reyes@olelife.com>
2026-03-17 17:33:57 -0500

    gg

--- model/revenue.js
+++ model/revenue.js
@@ -1,12 +1,62 @@
 cube(`revenue`, {
   sql: `SELECT * FROM olelifetech.gold_zone.fact_full_company_quotation_policies`,
 
+ /// measures: {
+ ///   revenue: {
+ ///     type: `sum`,
+ ///     sql: `annual_premium`
+ ///   }
+ /// },
+
   measures: {
-    revenue: {
-      type: `sum`,
-      sql: `annual_premium`
-    }
-  },
+
+      approved_apps: {
+        type: `number`,
+        sql: `
+          COUNT(DISTINCT CASE 
+            WHEN approval_date IS NOT NULL 
+            THEN nro_application 
+          END)
+        `
+      },
+
+      activated_policies: {
+        type: `number`,
+        sql: `
+          COALESCE(
+            COUNT(DISTINCT CASE 
+              WHEN payment_date IS NOT NULL 
+                AND status IN ('ACTIVE', 'GRACE PERIOD')
+              THEN nro_application 
+            END)
+          , 0)
+        `
+      },
+
+      activated_prm: {
+        type: `number`,
+        sql: `
+          COALESCE(
+            SUM(CASE 
+              WHEN payment_date IS NOT NULL 
+                AND status IN ('ACTIVE', 'GRACE PERIOD')
+              THEN annual_premium 
+            END)
+          , 0)
+        `
+      },
+
+      approved_prm: {
+        type: `number`,
+        sql: `
+          SUM(CASE 
+            WHEN approval_date IS NOT NULL 
+            THEN annual_premium 
+          END)
+        `
+      }
+
+    },
 
   dimensions: {
     createdAt: {
